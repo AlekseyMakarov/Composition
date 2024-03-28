435889d206fc95763b76cd794a3732eb99675fe5
Aleksey <leha2546461@rambler.ru>
2024-03-28 18:52:03 +0300

    Game finished fragment is realised

--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -5,19 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE
 import com.example.composition.R
-import com.example.composition.databinding.FragmentChooseLevelBinding
 import com.example.composition.databinding.FragmentGameFinishedBinding
 import com.example.composition.domain.entity.GameResult
-
 
 class GameFinishedFragment : Fragment() {
 
     private lateinit var gameResult: GameResult
     private val binding: FragmentGameFinishedBinding
-    get() = _binding?: throw RuntimeException("FragmentGameFinishedBinding is null")
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding is null")
     private var _binding: FragmentGameFinishedBinding? = null
 
     override fun onCreateView(
@@ -35,11 +34,40 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        backPressed()
+        addCallbackOnBackPressed()
         binding.buttonRetry.setOnClickListener {
             requireActivity().supportFragmentManager
                 .popBackStack(GameFragment.GAME_FRAGMENT_TAG, POP_BACK_STACK_INCLUSIVE)
         }
+        binding.tvRequiredAnswers.apply {
+            text = String.format(text.toString(), gameResult.gameSettings.minCountOfRightAnswers)
+        }
+        binding.tvRequiredPercentage.apply {
+            text = String.format(
+                text.toString(),
+                gameResult.gameSettings.minPercentageOfRightAnswers
+            )
+        }
+        binding.tvScoreAnswers.apply {
+            text = String.format(
+                text.toString(),
+                gameResult.countOfRightAnswers
+            )
+        }
+        binding.tvScorePercentage.apply {
+            text = String.format(
+                text.toString(),
+                gameResult.run {
+                    ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+                }
+            )
+        }
+        binding.emojiResult.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireContext(),
+                if (gameResult.winner) R.drawable.ic_smile else R.drawable.ic_sad
+            )
+        )
     }
 
     override fun onDestroyView() {
@@ -47,12 +75,12 @@ class GameFinishedFragment : Fragment() {
         _binding = null
     }
 
-    private fun parseArgs(){
+    private fun parseArgs() {
         gameResult = requireArguments().getParcelable<GameResult>(GAME_RESULT_KEY)!!
     }
 
-    private fun backPressed(){
-        requireActivity().onBackPressedDispatcher.addCallback(this.viewLifecycleOwner, object:
+    private fun addCallbackOnBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(this.viewLifecycleOwner, object :
             OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 retryGame()
@@ -60,15 +88,15 @@ class GameFinishedFragment : Fragment() {
         })
     }
 
-    private fun retryGame(){
+    private fun retryGame() {
         requireActivity().supportFragmentManager
             .popBackStack(GameFragment.GAME_FRAGMENT_TAG, POP_BACK_STACK_INCLUSIVE)
     }
 
-    companion object{
+    companion object {
         private const val GAME_RESULT_KEY = "game_result"
 
-        fun newInstance(gameResult: GameResult): GameFinishedFragment{
+        fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply {
                     putParcelable(GAME_RESULT_KEY, gameResult)
